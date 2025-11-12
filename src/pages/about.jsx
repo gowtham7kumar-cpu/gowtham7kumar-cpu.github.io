4eae7db164ae1e50f17e6906696e8dd234fba2ee
gowtham kumar <goku@gowthams-MacBook-Air.local>
2025-11-12 20:14:19 +0530

    Build with vite base '/evoke-films/' and redeploy

--- src/pages/about.jsx
+++
@@ -1,8 +0,0 @@
-export default function About(){
-  return (
-    <section>
-      <h1>About Us</h1>
-      <p>We craft films and campaigns that connect brands with emotion.</p>
-    </section>
-  );
-}
