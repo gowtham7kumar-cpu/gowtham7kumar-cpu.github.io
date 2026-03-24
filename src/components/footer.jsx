587519dba40ac1664a7128057af2957adc2a95f0
Goku1211 <gowtham7kumar@gmail.com>
2026-03-24 12:50:51 +0530

    Delete src/components directory

--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="border-t">
-      <div className="mx-auto max-w-6xl px-4 py-8 text-center text-sm text-neutral-600">
-        © {new Date().getFullYear()} Evoke Films. All rights reserved.
-      </div>
-    </footer>
-  );
-}
