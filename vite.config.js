b17ee9e206e22ea6457c8911cb9fd4bc0342ea5e
Goku1211 <gowtham7kumar@gmail.com>
2026-03-24 13:15:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
+  base: '/gowtham7kumar-cpu.github.io/',
   plugins: [react()],
 })
